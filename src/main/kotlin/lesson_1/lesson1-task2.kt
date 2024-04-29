319d25fbbabffd8fa8453ad4d577bb51d8bd9670
Ivan <groovst@gmail.com>
2024-04-29 17:57:39 +0500

    KP-lesson1-task2 completed task number two

--- src/main/kotlin/lesson_1/lesson1-task2.kt
+++ src/main/kotlin/lesson_1/lesson1-task2.kt
@@ -2,7 +2,7 @@ package org.example.lesson_1
 
 fun main() {
     var numberOfOrders:Int = 75
-    val thanksForPurchase: String = "Спасибо за покупку Приходите ещё =)"
+    val thanksForPurchase: String = "Спасибо за покупку Приходите ещё  =)"
 
     println(numberOfOrders)
     println(thanksForPurchase)
