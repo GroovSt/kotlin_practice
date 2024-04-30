32e7ff85e7b0b4c59a9e93351cffb569fe085480
Ivan <groovst@gmail.com>
2024-04-30 19:53:42 +0500

    KP-lesson2-task3 fix comment

--- src/main/kotlin/lesson_2/lesson2_task3.kt
+++ src/main/kotlin/lesson_2/lesson2_task3.kt
@@ -3,7 +3,9 @@ package org.example.lesson_2
 const val TRANSLATE_OF_TIME = 60
 
 fun main() {
-    val departureTimeInMinute = 9 * TRANSLATE_OF_TIME + 39 // перевел время отправления поезда в минуты
+    val startHour = 9
+    val startMinute = 39
+    val departureTimeInMinute = startHour * TRANSLATE_OF_TIME + startMinute // перевел время отправления поезда в минуты
     val travelTime = 457
 
     val arrivalTime = departureTimeInMinute + travelTime // время прибытия поезда переведенные в минуты
