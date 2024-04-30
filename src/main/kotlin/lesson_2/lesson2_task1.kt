4cb5af5cb0dd4816f0ca9dd921d3685c8c2aa13e
Ivan <groovst@gmail.com>
2024-04-30 14:57:48 +0500

    KP-lesson2-task1 fixes in variable avgScore

--- src/main/kotlin/lesson_2/lesson2_task1.kt
+++ src/main/kotlin/lesson_2/lesson2_task1.kt
@@ -6,7 +6,7 @@ fun main() {
     val student3 = 3
     val student4 = 5
 
-    val avgScore: Double = ((student1 + student2 + student3 + student4) / 4).toDouble()
+    val avgScore = (student1 + student2 + student3 + student4) / 4.0
 
     println("%.2f".format(avgScore))
 }
