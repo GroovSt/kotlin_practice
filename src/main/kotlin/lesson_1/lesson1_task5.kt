e1346be1760b9a1904cabcbcdae7a0ec02216916
Ivan <groovst@gmail.com>
2024-04-30 16:15:53 +0500

    KP-lesson1-task5 fix comments

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -1,10 +1,12 @@
 package org.example.lesson_1
 
+const val TRANSLATE_OF_TIME = 60
+
 fun main() {
     val secondsInSpace = 6480
-    val second = secondsInSpace % 60
-    val minute = secondsInSpace / 60 % 60
-    val hour = secondsInSpace / 60 / 60
+    val second = secondsInSpace % TRANSLATE_OF_TIME
+    val minute = secondsInSpace / TRANSLATE_OF_TIME % TRANSLATE_OF_TIME
+    val hour = secondsInSpace / TRANSLATE_OF_TIME / TRANSLATE_OF_TIME
 
     println("%02d:%02d:%02d".format(hour, minute, second))
 }
