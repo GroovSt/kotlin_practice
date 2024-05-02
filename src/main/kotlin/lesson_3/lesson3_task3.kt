cca1566560a7b339de23c2016d2349ce5fad205b
Ivan <groovst@gmail.com>
2024-05-02 14:59:05 +0500

    KP-lesson3-task2 completed task number three

---
+++ src/main/kotlin/lesson_3/lesson3_task3.kt
@@ -0,0 +1,17 @@
+package org.example.lesson_3
+
+fun main() {
+    val firstNum = 9
+
+    println("""
+        $firstNum x 1 = ${firstNum * 1}
+        $firstNum x 2 = ${firstNum * 2}
+        $firstNum x 3 = ${firstNum * 3}
+        $firstNum x 4 = ${firstNum * 4}
+        $firstNum x 5 = ${firstNum * 5}
+        $firstNum x 6 = ${firstNum * 6}
+        $firstNum x 7 = ${firstNum * 7}
+        $firstNum x 8 = ${firstNum * 8}
+        $firstNum x 9 = ${firstNum * 9}
+    """.trimIndent())
+}
