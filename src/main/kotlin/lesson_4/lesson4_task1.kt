a1ca95d00a0ba8d2622b864a9d2b0113434bf0df
Ivan <groovst@gmail.com>
2024-05-02 16:47:08 +0500

    KP-lesson4-task1 completed task number one

--- src/main/kotlin/lesson_4/lesson4_task1.kt
+++ src/main/kotlin/lesson_4/lesson4_task1.kt
@@ -1,2 +1,11 @@
 package org.example.lesson_4
 
+const val NUMBER_OF_TABLES = 13
+
+fun main() {
+    val bookedToday = NUMBER_OF_TABLES
+    val bookedTomorrow = NUMBER_OF_TABLES - 4
+
+    println("Доступность столиков на сегодня: ${bookedToday < NUMBER_OF_TABLES}")
+    println("Доступность столиков на завтра: ${bookedTomorrow < NUMBER_OF_TABLES}")
+}
