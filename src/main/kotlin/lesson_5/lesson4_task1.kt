ae30cf29df377594a5a8f074fa18ecf87ae1ee19
Ivan <groovst@gmail.com>
2024-05-03 19:33:36 +0500

    KP-lesson5-task1 completed task number one

--- src/main/kotlin/lesson_5/lesson4_task1.kt
+++ src/main/kotlin/lesson_5/lesson4_task1.kt
@@ -1,2 +1,12 @@
 package org.example.lesson_5
 
+fun main() {
+    println("Для того чтобы выяснить, что вы человек, напишите ответ для выражения: 150 + 150")
+    val example = 300
+    val answer = readln().toInt()
+
+    if (answer == example)
+        println("Добро пожаловать!")
+    else
+        println("Доступ запрещен.")
+}
