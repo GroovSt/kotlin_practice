7ea3565309699c54ac1bdc0e3936f066c748bf15
Ivan <groovst@gmail.com>
2024-05-09 13:20:43 +0500

    KP-lesson10-task1 completed task number one

---
+++ src/main/kotlin/lesson_10/lesson10_task1.kt
@@ -0,0 +1,30 @@
+package org.example.lesson_10
+
+fun main() {
+    var manThrow = 0
+    var computerThrow = 0
+
+    do {
+        println("Бросает человек!")
+        manThrow = rollTheDice()
+        println("Результат человека: $manThrow!")
+
+        println("Бросает компьютер!")
+        computerThrow = rollTheDice()
+        println("Результат компьютер: $computerThrow!")
+
+        if (manThrow > computerThrow)
+            println("Победило человечество")
+        else if (manThrow < computerThrow)
+            println("Победила машина")
+        else
+            println("Ничья, кидаем заново")
+
+    } while (manThrow == computerThrow)
+
+}
+
+fun rollTheDice(): Int {
+    val diceRoll = (1..6).random()
+    return diceRoll
+}
