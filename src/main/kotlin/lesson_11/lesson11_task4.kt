cb52fed1a0d72e63ff5a191dfdbc105f94c3f5b2
Ivan <groovst@gmail.com>
2024-05-14 17:03:29 +0500

    KP-lesson11-task4 completed task number four

---
+++ src/main/kotlin/lesson_11/lesson11_task4.kt
@@ -0,0 +1,27 @@
+package org.example.lesson_11
+
+import java.awt.Button
+
+class RecipeCategory(
+    val categoryName: String,
+    val categoryIcon: String,
+    val recipeName: String,
+    val recipeIcon: String,
+    val inFavoritesButton: Button,
+    val inFavorites: Boolean = false,
+    val categoryButton: Button,
+) {}
+
+class Recipe(
+    val recipeName: String,
+    val recipeIcon: String,
+    val inFavoritesButton: Button,
+    val inFavorites: Boolean = false,
+    val categoryButton: Button,
+) {}
+
+class Ingredient(
+    val numberOfServings: Int,
+    val ingredientName: MutableList<String>,
+    val amountOfIngredient: MutableList<String>,
+) {}
