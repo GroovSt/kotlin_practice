73cf3f8381c6609b92e58cb85acced4a115c5690
Ivan <groovst@gmail.com>
2024-05-20 19:24:51 +0500

    KP-lesson11-task3 fix comment

--- src/main/kotlin/lesson_11/lesson11_task3.kt
+++ src/main/kotlin/lesson_11/lesson11_task3.kt
@@ -3,13 +3,15 @@ package org.example.lesson_11
 class Room(
     val cover: String,
     val roomName: String,
-    val listOfParticipants: MutableList<String> = mutableListOf(),
+    val members: MutableList<Member> = mutableListOf(),
 ) {
-    fun addParticipant(participant: String) {
-        listOfParticipants.add(participant)
+    fun addMember(member: Member) {
+        members.add(member)
     }
 
     fun updateStatus(username: String, status: String) {
+        val member = members.find { it.username == username }
+        member?.status = status
         println("У пользователя $username новый статус: $status")
     }
 
@@ -18,19 +20,45 @@ class Room(
     }
 }
 
+class Member(
+    val username: String,
+    var status: String = "микрофон выключен", // По умолчанию статус "микрофон выключен"
+    val avatarUrl: String = "", // URL аватарки участника
+)
+
 fun main() {
     val room = Room(
         "cover.jpg",
         "Best room ever!",
     )
 
-    room.addParticipant("Ivan")
-    room.addParticipant("Sergey")
-    room.addParticipant("Irina")
+    room.addMember(
+        Member(
+            "Ivan",
+            "разговаривает",
+            "ivan_avatar.jpg",
+        )
+    )
+    room.addMember(
+        Member(
+            "Sergey",
+            "микрофон выключен",
+            "sergey_avatar.jpg",
+        )
+    )
+    room.addMember(
+        Member(
+            "Irina",
+            "пользователь заглушен",
+            "irina_avatar.jpg",
+        )
+    )
 
+    // Обновляем статусы участников
     room.updateStatus("Ivan", "разговаривает")
     room.updateStatus("Sergey", "микрофон выключен")
     room.updateStatus("Irina", "пользователь заглушен")
 
+    // Отображаем информацию о комнате и участниках
     room.displayUserInfo("Ivan")
 }
