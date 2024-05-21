c5e1b81a0d7403554b67a406d4dc4596b1e63e3b
Ivan <groovst@gmail.com>
2024-05-21 17:04:05 +0500

    KP-lesson12-task1 completed task number two and fix conflict

---
+++ src/main/kotlin/lesson_12/lesson12_task2.kt
@@ -0,0 +1,13 @@
+package org.example.lesson_12
+
+class TheWeather(var dayWeather: Int, var nightWeather: Int, var precipitation: Boolean) {
+}
+
+fun main() {
+    val day1 = TheWeather(45, 0, true)
+
+    println("Погода днем в первый день: ${day1.dayWeather}")
+    println("Погода ночью в первый день: ${day1.nightWeather}")
+    println("Осадки: ${day1.precipitation}")
+}
+
